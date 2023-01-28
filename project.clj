0379c9e74e7aa9fe61fdddb80e980670c2cba703
Anthony Hrvojic <anth@dezoris.net>
2023-01-28 12:36:40 -0500

    Move project to Leiningen; remove unneeded config

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject annapolis "0.1.0-SNAPSHOT"
+  :description "Kubernetes distributed application lab"
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [com.brunobonacci/mulog "0.9.0"]])
